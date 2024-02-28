34ed9b262d1b7cbaf0f7fc62e140f1a73af077cc
Sytone <github@sytone.com>
2024-02-28 11:48:18 -0800

    added return after command

--- macros/sytone.ts
+++ macros/sytone.ts
@@ -3,13 +3,13 @@ export function prepare(newMacro) {
 
     const macroExtensions = {
         "dance_SINGLETAP_A": newMacro()
-            .typeAlphanumeric("git status"),
+            .typeAlphanumeric("git status\n"),
         "dance_SINGLEHOLD_A": newMacro()
-            .typeAlphanumeric("git log"),
+            .typeAlphanumeric("git log\n"),
         "dance_DOUBLETAP_A": newMacro()
-            .typeAlphanumeric("ls -al"),
+            .typeAlphanumeric("ls -al\n"),
         "dance_DOUBLEHOLD_A": newMacro()
-            .typeAlphanumeric("cp ../moonlander_hacked.bin /mnt/c/tools/"),
+            .typeAlphanumeric("cp ../moonlander_hacked.bin /mnt/c/tools/\n"),
         //        case SINGLE_TAP: SEND_STRING("{}"); register_code(KC_LEFT); unregister_code(KC_LEFT); break;
     };
 
